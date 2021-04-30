6d41e17cef1a0f182048ccaed088c2c006e44523
Joaquin Morales <joaquin.moralestr@gmail.com>
2021-04-30 00:16:30 -0400

    Se establecen las variables que usaremos en main.js y las funciones basicas que tendras cada operador

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const suma = (a,b) => a + b; 
+const resta = (a,b) => a - b;
+const multiplicacion = (a,b) => a * b;
+const division = (a,b) => a / b;
+
+const siete = document.getElementById('siete').innerHTML;
+const ocho = document.getElementById('ocho').innerHTML;
+const nueve = document.getElementById('nueve').innerHTML;
+const cuatro = document.getElementById('cuatro').innerHTML;
+const cinco = document.getElementById('cinco').innerHTML;
+const seis = document.getElementById('seis').innerHTML;
+const uno = document.getElementById('uno').innerHTML;
+const dos = document.getElementById('dos').innerHTML;
+const tres = document.getElementById('tres').innerHTML;
+const cero = document.getElementById('cero').innerHTML;
+const c = document.getElementById('c').innerHTML;
+const multiplicar = document.getElementById('multiplicar').innerHTML;
+const dividir = document.getElementById('dividir').innerHTML;
+const sumar = document.getElementById('sumar').innerHTML;
+const restar = document.getElementById('restar').innerHTML;
+
