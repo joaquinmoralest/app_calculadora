922bc05d85572cd034e65a616eefb902a9f4572e
Joaquin Morales <joaquin.moralestr@gmail.com>
2021-05-01 22:40:10 -0400

    Se agregan los botones de igual y punto

--- js/main.js
+++ js/main.js
@@ -3,19 +3,27 @@ const resta = (a,b) => a - b;
 const multiplicacion = (a,b) => a * b;
 const division = (a,b) => a / b;
 
-const siete = document.getElementById('siete').innerHTML;
-const ocho = document.getElementById('ocho').innerHTML;
-const nueve = document.getElementById('nueve').innerHTML;
-const cuatro = document.getElementById('cuatro').innerHTML;
-const cinco = document.getElementById('cinco').innerHTML;
-const seis = document.getElementById('seis').innerHTML;
-const uno = document.getElementById('uno').innerHTML;
-const dos = document.getElementById('dos').innerHTML;
-const tres = document.getElementById('tres').innerHTML;
-const cero = document.getElementById('cero').innerHTML;
-const c = document.getElementById('c').innerHTML;
-const multiplicar = document.getElementById('multiplicar').innerHTML;
-const dividir = document.getElementById('dividir').innerHTML;
-const sumar = document.getElementById('sumar').innerHTML;
-const restar = document.getElementById('restar').innerHTML;
+const barraResultado = document.getElementById('barra');
+const siete = document.getElementById('siete');
+const ocho = document.getElementById('ocho');
+const nueve = document.getElementById('nueve');
+const cuatro = document.getElementById('cuatro');
+const cinco = document.getElementById('cinco');
+const seis = document.getElementById('seis');
+const uno = document.getElementById('uno');
+const dos = document.getElementById('dos');
+const tres = document.getElementById('tres');
+const cero = document.getElementById('cero');
+const c = document.getElementById('c');
+const multiplicar = document.getElementById('multiplicar');
+const dividir = document.getElementById('dividir');
+const sumar = document.getElementById('sumar');
+const restar = document.getElementById('restar');
+const igual = document.getElementById('igusl');
+const punto = document.getElementById('punto');
 
+siete.onclick = () => {
+    barraResultado = siete.innerHTML;
+};
+
+console.log(siete.innerHTML);
